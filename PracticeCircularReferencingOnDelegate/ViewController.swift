ca409b811f47be4d3e2893e0ff1adede7fb6bf0a
0102johnny <rt2824.recruit.1022@gmail.com>
2022-05-24 17:38:33 +0900

    起動画面のUIを作成

--- PracticeCircularReferencingOnDelegate/ViewController.swift
+++ PracticeCircularReferencingOnDelegate/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func showSecondVC(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
