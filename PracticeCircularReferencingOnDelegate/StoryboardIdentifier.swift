b616579ceb34d3524c1f60758f63c6d109e49e1c
0102johnny <rt2824.recruit.1022@gmail.com>
2022-05-24 17:54:11 +0900

    StoryboardのNameとIdを管理するenumをそれぞれ生成

---
+++ PracticeCircularReferencingOnDelegate/StoryboardIdentifier.swift
@@ -0,0 +1,12 @@
+//
+//  StoryboardIdentifier.swift
+//  PracticeCircularReferencingOnDelegate
+//
+//  Created by Johnny Toda on 2022/05/24.
+//
+
+import Foundation
+
+enum StoryboardIdentifier {
+    static let second = "Second"
+}
---
+++ PracticeCircularReferencingOnDelegate/StoryboardName.swift
@@ -0,0 +1,12 @@
+//
+//  StoryboardName.swift
+//  PracticeCircularReferencingOnDelegate
+//
+//  Created by Johnny Toda on 2022/05/24.
+//
+
+import Foundation
+
+enum StoryboardName {
+    static let second = "Second"
+}
